8dfe23ad8ae9eb45185afc999253b7e3e42fca02
Timur Ermeshev <107412699+ermetim@users.noreply.github.com>
2025-03-13 13:30:32 +0300

    Create docker-compose, init.sql, index.php

---
+++ init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE my_table (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO my_table (name) VALUES ('Apple');
+INSERT INTO my_table (name) VALUES ('Banana');
+INSERT INTO my_table (name) VALUES ('Cherry');
